b7e570be597080c0a11d3c25944efa889ab6df39
SangJae Lee <dev.codemaestro@gmail.com>
2024-10-06 01:24:57 +0900

    [Bronze V] Title: 윤년, Time: 96 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("dev/stdin").toString().trim();
+
+const a = parseInt(input);
+
+if (a % 4 === 0) {
+  if (a % 400 === 0 || a % 100 !== 0) {
+    console.log("1");
+  } else {
+    console.log("0");
+  }
+} else {
+  console.log("0");
+}
