22b71709f1c174c7380e3566dc73902b66696afa
SangJae Lee <dev.codemaestro@gmail.com>
2024-10-14 16:50:26 +0900

    [Bronze V] Title: A+B - 4, Time: 100 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const maxNum = input.length-1;
+for (let i = 0; i <= maxNum; i++) {
+  const [A, B] = input[i].split(" ").map(Number);
+  console.log(A + B);
+}
