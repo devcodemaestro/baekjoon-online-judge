47e915a70f22443e80129b0fcace05bdf663e5e6
SangJae Lee <dev.codemaestro@gmail.com>
2024-10-17 11:18:23 +0900

    [Bronze III] Title: 최소, 최대, Time: 360 ms, Memory: 96448 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.js
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -3,16 +3,9 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const maxLength = Number(input[0]);
-const arr = input[1].split(" ");
-let maxNum = -1000000;
-let minNum = 1000000;
+const arr = input[1].split(" ").map(Number);
+let str = "";
+str = str + Math.min(...arr);
+str = str + " " + Math.max(...arr);
 
-for (let i = 0; i < maxLength; i++) {
-  if (Number(arr[i]) > maxNum) {
-    maxNum = arr[i];
-  }
-  if (Number(arr[i]) < minNum) {
-    minNum = arr[i];
-  }
-}
-console.log(minNum, maxNum);
+console.log(str);
