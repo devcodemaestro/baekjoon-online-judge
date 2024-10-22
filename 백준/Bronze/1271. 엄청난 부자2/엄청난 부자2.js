61c6503222b900c747a5da7b36a2a76ab9c1e7ec
SangJae Lee <dev.codemaestro@gmail.com>
2024-10-22 11:08:19 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 88 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const [A, B] = fs
+  .readFileSync("dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(BigInt);
+
+console.log((A / B).toString());
+console.log((A % B).toString());
