58d35411be78da15945d64afa1eacb1bc61c5d8d
SangJae Lee <dev.codemaestro@gmail.com>
2024-10-22 10:18:57 +0900

    [Silver III] Title: 1, Time: 344 ms, Memory: 11788 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
+
+let str = "";
+for (let i = 0; i < input.length; i++) {
+  const divisor = BigInt(input[i]);
+  let target = BigInt(11);
+  let length = 2;
+  if (divisor === 1n) {
+    str += "1\n";
+  } else {
+    while (target % divisor !== 0n) {
+      target = target * 10n + 1n;
+      length++;
+    }
+    str += length + "\n";
+  }
+}
+str = str.substring(0, str.length - 1);
+console.log(str);
