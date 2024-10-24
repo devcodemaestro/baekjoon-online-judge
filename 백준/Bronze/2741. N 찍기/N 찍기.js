7183010954984bff078a2f3f21c0c799a9af974e
SangJae Lee <dev.codemaestro@gmail.com>
2024-10-24 09:35:16 +0900

    [Bronze V] Title: N 찍기, Time: 192 ms, Memory: 22112 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.js
+++ 백준/Bronze/2741. N 찍기/N 찍기.js
@@ -1,12 +1,5 @@
 const fs = require("fs");
-const maxNum = Number(fs.readFileSync("/dev/stdin").toString().trim());
-let str = "";
-for (let i = 1; i <= maxNum; i++) {
-  if (i !== maxNum) {
-    str += i + "\n";
-  } else {
-    str += i;
-  }
-}
+const input = fs.readFileSync("/dev/stdin").toString();
 
-console.log(str);
+const arr = Array.from({ length: input }, (v, i) => i + 1);
+console.log(arr.join("\n"));
