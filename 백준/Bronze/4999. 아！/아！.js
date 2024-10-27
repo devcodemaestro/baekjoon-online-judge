3421366d421fcda94bc7215faf9496e4f4a85020
SangJae Lee <dev.codemaestro@gmail.com>
2024-10-27 16:49:16 +0900

    [Bronze V] Title: 아!, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/4999. 아！/아！.js
+++ 백준/Bronze/4999. 아！/아！.js
@@ -4,22 +4,5 @@ const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 let jaehwan = input[0].split("");
 let doctor = input[1].split("");
-let count = 0;
 
-for (let i = 0; i < jaehwan.length; i++) {
-  if (jaehwan[i] === "a") {
-    count++;
-  }
-}
-jaehwan = count;
-
-count = 0;
-
-for (let i = 0; i < doctor.length; i++) {
-  if (doctor[i] === "a") {
-    count++;
-  }
-}
-doctor = count;
-
-jaehwan < doctor ? console.log("no") : console.log("go");
+jaehwan.length < doctor.length ? console.log("no") : console.log("go");
