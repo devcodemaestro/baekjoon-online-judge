3ba92e5047488ff0b8f27c648b8c4fbabd75d19a
SangJae Lee <dev.codemaestro@gmail.com>
2024-11-05 06:18:05 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.54 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,14 +1,7 @@
 function solution(arr, queries) {
-    let prev
-    let i
-    let j
-    for(let idx = 0; idx < queries.length; idx++){
-        i = queries[idx][0]
-        j = queries[idx][1]
-        prev = arr[i]
-        arr[i] = arr[j]
-        arr[j] = prev
-    }
+    queries.forEach(([i,j])=> {
+        [arr[i],arr[j]] = [arr[j],arr[i]]
+    })
     var answer = arr
     return answer;
 }
