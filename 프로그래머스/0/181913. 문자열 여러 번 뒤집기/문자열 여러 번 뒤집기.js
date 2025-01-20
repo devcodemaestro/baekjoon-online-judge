4188ef916aef8767926e05f84e4a781433421dae
SangJae Lee <dev.codemaestro@gmail.com>
2025-01-21 06:31:34 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 21.90 ms, Memory: 35 MB -BaekjoonHub

---
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -0,0 +1,14 @@
+function solution(my_string, queries) {
+    var answer = '';
+    let arr = []
+    let str = my_string
+    for(let i = 0; i < queries.length; i ++){
+        let [s, e] = queries[i]
+        arr = str.split("")
+        let reverse = arr.slice(s,e+1).reverse()
+        // 검증 끝
+        arr.splice(s,(e - s + 1), ...reverse)
+        str = arr.join("")
+    }        
+    return str;
+}
