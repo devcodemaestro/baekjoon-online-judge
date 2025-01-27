247f9dbdcd1b7740a921973986665ec9e15777d9
SangJae Lee <dev.codemaestro@gmail.com>
2025-01-27 23:23:17 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,15 +1,11 @@
 function solution(str_list) {
-    var answer = []
-    if(str_list.length===1 && (str_list.indexOf("l") === -1 && str_list.indexOf("r") === -1) ){
-        return []
-    } else{
-        let l = str_list.indexOf("l") === -1 ? 30 : str_list.indexOf("l")        
-        let r = str_list.indexOf("r") === -1 ? 30 : str_list.indexOf("r")
-        if(l < r){
-            return str_list.slice(0, l)
-        }else{
-            return str_list.slice(r+1)
+    for(let i = 0; i < str_list.length; i++){
+        if(str_list[i] === "l"){
+            return str_list.slice(0, i)
+        }
+        if(str_list[i] === "r"){
+            return str_list.slice(i+1)
         }
     }
-    return answer
+    return []
 }
