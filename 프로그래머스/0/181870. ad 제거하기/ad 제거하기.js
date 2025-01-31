fac60166dccecbd64a19a689c734c15a63c55c64
SangJae Lee <dev.codemaestro@gmail.com>
2025-01-31 16:10:12 +0900

    [level 0] Title: ad 제거하기, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.js
@@ -0,0 +1,9 @@
+function solution(strArr) {
+    var answer = [];
+    for(let i = 0; i < strArr.length; i++){
+        if(strArr[i].indexOf("ad") === -1){
+            answer.push(strArr[i])
+        }
+    }
+    return answer;
+}
