19119e4d4e0dfc8a7f00645a1042be930386b927
SangJae Lee <dev.codemaestro@gmail.com>
2025-02-03 14:44:20 +0900

    [level 0] Title: 문자열 묶기, Time: 6.73 ms, Memory: 51.1 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -1,18 +1,25 @@
 function solution(strArr) {
-    var answer = 0;
-    let obj = {}
-    for(let i = 0; i <strArr.length; i++){
-        if(obj[strArr[i].length] === undefined){
-            obj[strArr[i].length] = 1
-        }else{
-            obj[strArr[i].length]++
-        }
+    let arr = Array(31).fill(0)
+    
+    for(e of strArr){
+        arr[e.length]++
     }
-    let arr = Object.values(obj)
-    for(let j = 0; j <= arr.length; j++){
-        if(answer <= arr[j]){
-            answer = arr[j]            
-        }
-    }
-    return answer;
+    return Math.max(...arr);
 }
+    
+//  let obj = {}
+//     for(let i = 0; i <strArr.length; i++){
+//         if(obj[strArr[i].length] === undefined){
+//             obj[strArr[i].length] = 1
+//         }else{
+//             obj[strArr[i].length]++
+//         }
+//     }
+//     let arr = Object.values(obj)
+//     for(let j = 0; j <= arr.length; j++){
+//         if(answer <= arr[j]){
+//             answer = arr[j]            
+//         }
+//     }
+//     return answer;
+// }
