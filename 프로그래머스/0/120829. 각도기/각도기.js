c58d5dc9ce5e7d37fe07214273fc7ca187faca41
SangJae Lee <dev.codemaestro@gmail.com>
2025-02-09 01:05:24 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -0,0 +1,13 @@
+function solution(angle) {
+    if(angle === 90){
+        return 2
+    }
+    if(angle === 180){
+        return 4
+    }
+    if(angle < 90){
+        return 1
+    }else{
+        return 3 
+    }
+}
