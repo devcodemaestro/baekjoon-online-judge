47ec0e34a7d291ec08ef18c7c88b815688a669bf
SangJae Lee <dev.codemaestro@gmail.com>
2025-02-10 04:15:21 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,19 +1,15 @@
 function solution(hp) {
     var answer = 0;
-    while(hp >= 5){
-        let num = Math.floor(hp / 5)
+    let num1 = Math.floor(hp / 5)
         hp %= 5
-        answer+= num
-    }
-    while(hp >= 3){
-        let num = Math.floor(hp / 3)
+        answer+= num1
+    
+    let num2 = Math.floor(hp / 3)
         hp %= 3
-        answer += num
-    }
-    while(hp >= 1){
-        let num = Math.floor(hp / 1)
-        hp %=1
-        answer+= num
-    }
+        answer += num2
+    
+    let num3 = Math.floor(hp / 1)
+        answer+= num3
+    
     return answer;
 }
