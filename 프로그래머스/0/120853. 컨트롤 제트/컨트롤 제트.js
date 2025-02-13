0575c54b9b296a925effa5bdde05393c027e7d80
SangJae Lee <dev.codemaestro@gmail.com>
2025-02-14 00:42:27 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.30 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,13 +1,11 @@
 function solution(s) {
     var answer = 0;
-    let arr = s.split(" ").map((e)=>Number(e))
-    
-    for(let i = 0; i < arr.length; i++){
-        if(isNaN(arr[i])){
-            answer -= arr[i-1]
-        }else{
-            answer+= arr[i]
-        }
-    }
+    let arr = []
+    s.split(" ").map((e)=>Number(e)).forEach((e)=>{
+        isNaN(e) ? arr.pop() : arr.push(e)
+    })
+
+    arr.forEach((e)=> answer+=e)
+  
     return answer;
 }
