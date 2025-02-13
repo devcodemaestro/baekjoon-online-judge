8737047c39091be7c0c010c635cebd5aafd04061
SangJae Lee <dev.codemaestro@gmail.com>
2025-02-13 23:59:54 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    let answer = []
+    for(let i = 0; i < my_string.length; i++){
+        if(Number(my_string[i]) >= 0){
+            answer.push(Number(my_string[i]))
+        }
+    }
+    return answer.sort()
+}
